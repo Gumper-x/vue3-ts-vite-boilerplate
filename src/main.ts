745334cb6638127fc6ad75f495c0f46136d3513d
Azema4ka <co4nev.1081@mail.ru>
2021-08-01 20:43:37 +0300

    feat: add initApp function

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+function initApp(): void {
+  const app = createApp(App);
+  app.mount('#app')
+};
+
+initApp()
