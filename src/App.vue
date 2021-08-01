a467dd5a94c23a288cae8cce148529a6036f5d56
Azema4ka <co4nev.1081@mail.ru>
2021-08-01 20:43:14 +0300

    feat: remove component HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    App
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
 })
 </script>
 
