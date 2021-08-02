fc9381f7cf4700a489568f481397c66dc0160961
Azema4ka <co4nev.1081@mail.ru>
2021-08-02 23:35:31 +0300

    feat: init pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import { createPinia } from "pinia";
 import router from "@/router";
+import App from "./App.vue";
 
 function initApp(): void {
   const app = createApp(App);
   app.use(router);
+  app.use(createPinia());
   app.mount("#app");
 }
 
