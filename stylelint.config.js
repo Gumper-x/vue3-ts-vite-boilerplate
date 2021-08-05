4a3ad53a27d81bb5e4d4a594e8909d11c4c053e9
Azema4ka <co4nev.1081@mail.ru>
2021-08-06 00:24:59 +0300

    chore: add stylelint-scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ["stylelint-config-recommended", "stylelint-config-standard"],
-  plugins: ["stylelint-prettier"],
+  plugins: ["stylelint-scss", "stylelint-prettier"],
   rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
     "prettier/prettier": true,
   },
 };
