18f968278c910566dafb9759c3688946f44a638f
Gumper-x <chaneldeo2002@gmail.com>
2021-09-19 14:19:19 +0300

    feat: remove vite-plugin-checker and add lint:style cheking

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
-import checker from "vite-plugin-checker";
 import ViteVisualizer from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    checker({
-      vls: true,
-      vueTsc: true,
-      typescript: true,
-    }),
     ViteVisualizer({
       filename: "./dist/report.html",
       open: true,
