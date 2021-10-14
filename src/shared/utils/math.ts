f3a6385a9b553e8ed2f925c56457db064f520d8a
Azema4ka <co4nev.1081@mail.ru>
2021-10-14 13:18:20 +0300

    chore(shared): remove deprecated utils

--- src/shared/utils/math.ts
+++ src/shared/utils/math.ts
@@ -1,7 +1,3 @@
-export function random(min: number, max: number = min): number {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 export function isEven(value: number): boolean {
   return value % 2 === 0;
 }
